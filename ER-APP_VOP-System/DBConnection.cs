99a24f9a2918bfa6382ea9ff4d3cbac41a4fff79
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2023-03-01 16:06:01 +0530

    DBConnection added.Connection string changed.

---
+++ ER-APP_VOP-System/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ER_APP_VOP_System
+{
+    public class DBConnection
+    {
+        public static string ConnectionString = "Data Source=DESKTOP-DCCUTP6\\SQLEXPRESS; Initial Catalog=MAS_Synergy_HR; Integrated Security=true";
+        //User ID = YourUsername; Password=YourPassword;
+    }
+}
